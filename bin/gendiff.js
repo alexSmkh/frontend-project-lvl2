6e5355e80a9284739c7cbd5870ae3dedbdd554d3
alexSmkh <alexshikari9333@gmail.com>
2020-06-09 14:24:55 +0300

    Added an entry point

---
+++ bin/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import program from 'commander';
+import fn from '../src/index.js';
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, options) => {
+    fn(options.format, filepath1, filepath2);
+  })
+  .parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export default (format, path1, path2) => {
+  console.log(`format: ${format}\npath1: ${path1}\npath2: ${path2}`);
+};
