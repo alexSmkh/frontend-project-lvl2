474bad3cb72347d05fef6ebc34899100e212d6bf
alexSmkh <alexshikari9333@gmail.com>
2020-07-25 21:44:26 +0300

    Added default formatter

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,12 +3,13 @@
 import program from 'commander';
 import gendiff from '../index.js';
 
+
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(gendiff(filepath1, filepath2));
+  .action((filepath1, filepath2, option) => {
+    console.log(gendiff(filepath1, filepath2, option.format));
   })
   .parse(process.argv);
