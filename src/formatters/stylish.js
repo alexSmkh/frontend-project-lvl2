b1a0245470716cfe7c8e9ed9571a5346e3285b8c
alexSmkh <alexshikari9333@gmail.com>
2020-07-26 19:58:22 +0300

    Changed formatting logic

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,69 +1,58 @@
 import lodash from 'lodash';
 
-const { isPlainObject } = lodash;
+const { isPlainObject, isArray } = lodash;
 
 const baseIndent = '  ';
 
-const stringPatterns = [
-  {
-    check: (typeOfChange) => typeOfChange === 'unchanged',
-    makeStringOfChanges: (key, indent, previousValue) => `${indent}  ${key}: ${previousValue}`,
-  },
-  {
-    check: (typeOfChange) => typeOfChange === 'changed',
-    makeStringOfChanges: (key, indent, previousValue, currentValue) => `${indent}+ ${key}: ${currentValue}\n${indent}- ${key}: ${previousValue}`,
-  },
-  {
-    check: (typeOfChange) => typeOfChange === 'removed',
-    makeStringOfChanges: (key, indent, previousValue) => `${indent}- ${key}: ${previousValue}`,
-  },
-  {
-    check: (typeOfChange) => typeOfChange === 'added',
-    makeStringOfChanges: (key, indent, _, currentValue) => `${indent}+ ${key}: ${currentValue}`,
-  },
-  {
-    check: (typeOfChange) => typeOfChange === 'complex',
-    makeStringOfChanges: (key, indent, complexValue) => `${indent}  ${key}: {\n${complexValue}\n${indent}  }`,
-  },
-];
-
-const objToStr = (obj, depth) => {
+const formatValue = (value, depth) => {
+  if (!isPlainObject(value)) return value;
   const indent = baseIndent.repeat(depth);
   const result = Object
-    .keys(obj)
-    .reduce((acc, key) => {
-      const value = (isPlainObject(obj[key])) ? objToStr(obj[key], depth + 1) : obj[key];
-      return [...acc, `${indent}  ${key}: ${value}`];
-    }, ['{']);
+    .keys(value)
+    .reduce((acc, key) => (
+      [...acc, `${indent}  ${key}: ${formatValue(value[key], depth + 1)}`]
+    ), ['{']);
   result.push(`${baseIndent.repeat(depth - 1)}  }`);
   return result.join('\n');
 };
 
+const stringPatterns = [
+  {
+    check: (typeOfChange) => typeOfChange === 'unchanged',
+    makeStringOfChanges: (key, indent, value) => `${indent}  ${key}: ${value}`,
+  },
+  {
+    check: (typeOfChange) => typeOfChange === 'removed',
+    makeStringOfChanges: (key, indent, value) => `${indent}- ${key}: ${value}`,
+  },
+  {
+    check: (typeOfChange) => typeOfChange === 'added',
+    makeStringOfChanges: (key, indent, value) => `${indent}+ ${key}: ${value}`,
+  },
+  {
+    check: (typeOfChange) => typeOfChange === 'updated',
+    makeStringOfChanges: (key, indent, previous, current) => (
+      `${indent}+ ${key}: ${current}\n${indent}- ${key}: ${previous}`
+    ),
+  },
+];
+
 export default (ast) => {
-  const iter = (currentAst, depth) => {
-    const currentIndent = baseIndent.repeat(depth);
-    return Object
-      .keys(currentAst)
-      .reduce((acc, key) => {
-        const { typeOfChange, previousValue, currentValue } = currentAst[key];
-        const { makeStringOfChanges } = stringPatterns.find(({ check }) => check(typeOfChange));
-
-        if (typeOfChange === 'complex') {
-          const formattedComplexValue = iter(currentValue, depth + 1);
-          return [...acc, makeStringOfChanges(key, currentIndent, formattedComplexValue)];
-        }
-
-        const previous = (isPlainObject(previousValue))
-          ? objToStr(previousValue, depth + 1) : previousValue;
-        const current = (isPlainObject(currentValue))
-          ? objToStr(currentValue, depth + 1) : currentValue;
-        return [
-          ...acc,
-          makeStringOfChanges(key, currentIndent, previous, current),
-        ];
-      }, [])
-      .join('\n');
+  const iter = (node, depth) => {
+    const indent = baseIndent.repeat(depth);
+    const result = node.flatMap(({ key, typeOfChange, value }) => {
+      if (typeOfChange === 'updatedObject') {
+        return `${indent}  ${key}: {\n${iter(value, depth + 1).join('\n')}\n${indent}  }`;
+      }
+      const { makeStringOfChanges } = stringPatterns.find(({ check }) => check(typeOfChange));
+      if (isArray(value)) {
+        const previous = formatValue(value[0], depth + 1);
+        const current = formatValue(value[1], depth + 1);
+        return makeStringOfChanges(key, indent, previous, current);
+      }
+      return makeStringOfChanges(key, indent, formatValue(value, depth + 1));
+    });
+    return result;
   };
-  const result = iter(ast, 1);
-  return ['{', result, '}'].join('\n');
+  return ['{', ...iter(ast, 1), '}'].join('\n');
 };
