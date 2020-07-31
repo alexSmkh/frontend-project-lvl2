ff7369ae5ac51449a4924a4326b00be83e855776
alexSmkh <alexshikari9333@gmail.com>
2020-07-31 15:15:34 +0300

    Added a comment

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -9,9 +9,25 @@ const formatters = {
   json,
 };
 
+/*
+
+Похожий интерфей есть еще в src/parsers.js, на который ругается codeclimate(дублирование кода).
+Нужно ли выносить их в отдельную функию? Например:
+
+const func = (items, item, partOfErrorMessage)
+if (!_.has(items, item)) {
+  const availableItems = Object.keys(items).join(', ');
+  throw new Error(`"${item}" ${partOfErrorMessage} ${availableFormatters}.`);
+}
+return items[item];
+}
+
+Хотя вынос одного интерфейса на несколько модулей кажется странным решением =\
+*/
+
 export default (formatter) => {
   if (!_.has(formatters, formatter)) {
-    const availableFormatters = Object.keys(formatters).join(',');
+    const availableFormatters = Object.keys(formatters).join(', ');
     throw new Error(`"${formatter}" formatting is not supported!\nAvailable formatters: ${availableFormatters}.`);
   }
   return formatters[formatter];
