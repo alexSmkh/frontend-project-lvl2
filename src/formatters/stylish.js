1f46149daf84859a1405740a161278e4a2e15af4
alexSmkh <alexshikari9333@gmail.com>
2020-08-07 18:56:07 +0300

    Refactoring

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,69 +1,81 @@
 import lodash from 'lodash';
 
-const { isPlainObject } = lodash;
+const { isPlainObject, isArray } = lodash;
 
 const baseIndent = '  ';
 const openingBracket = '{';
 const closingBracket = '}';
 const depthIncrementStep = 2;
 
-const renderObject = (value, depth) => {
+const prepareValueForRender = (value, depth) => {
   if (!isPlainObject(value)) return value;
   const indent = baseIndent.repeat(depth);
   const result = Object
     .keys(value)
     .flatMap((key) => (
-      `${indent}  ${key}: ${renderObject(value[key], depth + depthIncrementStep)}`
+      `${indent}  ${key}: ${prepareValueForRender(value[key], depth + depthIncrementStep)}`
     ));
   const lastLine = `${baseIndent.repeat(depth - depthIncrementStep)}  ${closingBracket}`;
   return [openingBracket, result.join('\n'), lastLine].join('\n');
 };
 
+const nodesForRender = [
+  {
+    check: ({ children }) => children !== undefined,
+    makeNodeForRender: ({ key, type, children }, depth, func) => {
+      const value = func(children, depth + depthIncrementStep).join('\n');
+      return { key, type, value };
+    },
+  },
+  {
+    check: ({ value }) => isArray(value),
+    makeNodeForRender: ({ key, type, value }, depth) => {
+      const [valueBefore, valueAfter] = value.map(
+        (item) => prepareValueForRender(item, depth + depthIncrementStep),
+      );
+      return {
+        key, type, valueBefore, valueAfter,
+      };
+    },
+  },
+  {
+    check: () => true,
+    makeNodeForRender: ({ key, type, value }, depth) => (
+      { key, type, value: prepareValueForRender(value, depth + depthIncrementStep) }),
+  },
+];
+
 const stringPatterns = [
   {
     check: (type) => type === 'unchanged',
-    makeStringOfChanges: ({ key, value }, depth) => {
-      const indent = baseIndent.repeat(depth);
-      return `${indent}  ${key}: ${renderObject(value, depth + depthIncrementStep)}`;
-    },
+    makeStringOfChanges: ({ key, value }, indent) => `${indent}  ${key}: ${value}`,
   },
   {
     check: (type) => type === 'removed',
-    makeStringOfChanges: ({ key, value }, depth) => {
-      const indent = baseIndent.repeat(depth);
-      return `${indent}- ${key}: ${renderObject(value, depth + depthIncrementStep)}`;
-    },
+    makeStringOfChanges: ({ key, value }, indent) => `${indent}- ${key}: ${value}`,
   },
   {
     check: (type) => type === 'added',
-    makeStringOfChanges: ({ key, value }, depth) => {
-      const indent = baseIndent.repeat(depth);
-      return `${indent}+ ${key}: ${renderObject(value, depth + depthIncrementStep)}`;
-    },
+    makeStringOfChanges: ({ key, value }, indent) => `${indent}+ ${key}: ${value}`,
   },
   {
     check: (type) => type === 'updated',
-    makeStringOfChanges: ({ key, value }, depth) => {
-      const [valueBefore, valueAfter] = value;
-      const indent = baseIndent.repeat(depth);
-      return `${indent}- ${key}: ${renderObject(valueBefore, depth + depthIncrementStep)}\n${indent}+ ${key}: ${renderObject(valueAfter, depth + depthIncrementStep)}`;
-    },
+    makeStringOfChanges: ({ key, valueBefore, valueAfter }, indent) => `${indent}- ${key}: ${valueBefore}\n${indent}+ ${key}: ${valueAfter}`,
   },
   {
     check: (type) => type === 'complex',
-    makeStringOfChanges: ({ key, children }, depth, func) => {
-      const complexValue = func(children, depth + 2).join('\n');
-      const indent = baseIndent.repeat(depth);
-      return `${indent}  ${key}: ${openingBracket}\n${complexValue}\n${indent}  ${closingBracket}`;
-    },
+    makeStringOfChanges: ({ key, value }, indent) => `${indent}  ${key}: ${openingBracket}\n${value}\n${indent}  ${closingBracket}`,
   },
 ];
 
 export default (ast) => {
   const iter = (nodes, depth) => {
+    const indent = baseIndent.repeat(depth);
     const result = nodes.flatMap((node) => {
+      const { makeNodeForRender } = nodesForRender.find(({ check }) => check(node));
       const { makeStringOfChanges } = stringPatterns.find(({ check }) => check(node.type));
-      return makeStringOfChanges(node, depth, iter);
+      const nodeForRender = makeNodeForRender(node, depth, iter);
+      return makeStringOfChanges(nodeForRender, indent);
     });
     return result;
   };
