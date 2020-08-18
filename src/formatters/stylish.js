333329041867051675744fe811e0917c6f2a6df6
alexSmkh <alexshikari9333@gmail.com>
2020-08-19 00:20:46 +0300

    Rewrote the stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,16 +1,11 @@
 import lodash from 'lodash';
 
-const { isPlainObject, isArray, includes } = lodash;
+const { isPlainObject } = lodash;
 
 const baseIndent = '  ';
 const openingBracket = '{';
 const closingBracket = '}';
 const depthIncrementStep = 2;
-const signsForChanges = {
-  unchanged: ' ',
-  removed: '-',
-  added: '+',
-};
 
 const prepareValueForRender = (value, depth) => {
   if (!isPlainObject(value)) return value;
@@ -24,53 +19,41 @@ const prepareValueForRender = (value, depth) => {
   return [openingBracket, result.join('\n'), lastLine].join('\n');
 };
 
-const nodesForRender = [
-  {
-    check: ({ children }) => children !== undefined,
-    makeNodeForRender: ({ key, type, children }, depth, func) => {
-      const value = func(children, depth + depthIncrementStep).join('\n');
-      return { key, type, value };
-    },
-  },
-  {
-    check: ({ value }) => isArray(value),
-    makeNodeForRender: ({ key, type, value }, depth) => {
-      const [valueBefore, valueAfter] = value.map(
-        (item) => prepareValueForRender(item, depth + depthIncrementStep),
-      );
-      return {
-        key, type, valueBefore, valueAfter,
-      };
-    },
-  },
-  {
-    check: () => true,
-    makeNodeForRender: ({ key, type, value }, depth) => (
-      { key, type, value: prepareValueForRender(value, depth + depthIncrementStep) }),
-  },
-];
+const makeNodeForRender = (node, depth, func) => {
+  const {
+    key, type, value, children,
+  } = node;
 
-const stringPatterns = [
-  {
-    check: (type) => includes(Object.keys(signsForChanges), type),
-    makeStringOfChanges: ({ key, type, value }, indent) => `${indent}${signsForChanges[type]} ${key}: ${value}`,
-  },
-  {
-    check: (type) => type === 'updated',
-    makeStringOfChanges: ({ key, valueBefore, valueAfter }, indent) => `${indent}- ${key}: ${valueBefore}\n${indent}+ ${key}: ${valueAfter}`,
-  },
-  {
-    check: (type) => type === 'complex',
-    makeStringOfChanges: ({ key, value }, indent) => `${indent}  ${key}: ${openingBracket}\n${value}\n${indent}  ${closingBracket}`,
-  },
-];
+  if (type === 'complex') {
+    const complexValue = func(children, depth + depthIncrementStep).join('\n');
+    return { key, type, value: complexValue };
+  }
+
+  if (type === 'updated') {
+    const [valueBefore, valueAfter] = value.map(
+      (item) => prepareValueForRender(item, depth + depthIncrementStep),
+    );
+    return {
+      key, type, valueBefore, valueAfter,
+    };
+  }
+
+  return { key, type, value: prepareValueForRender(value, depth + depthIncrementStep) };
+};
+
+const stringPatterns = {
+  unchanged: ({ key, value }, indent) => `${indent}  ${key}: ${value}`,
+  added: ({ key, value }, indent) => `${indent}+ ${key}: ${value}`,
+  removed: ({ key, value }, indent) => `${indent}- ${key}: ${value}`,
+  updated: ({ key, valueBefore, valueAfter }, indent) => `${indent}- ${key}: ${valueBefore}\n${indent}+ ${key}: ${valueAfter}`,
+  complex: ({ key, value }, indent) => `${indent}  ${key}: ${openingBracket}\n${value}\n${indent}  ${closingBracket}`,
+};
 
 export default (ast) => {
   const iter = (nodes, depth) => {
     const indent = baseIndent.repeat(depth);
     const result = nodes.flatMap((node) => {
-      const { makeNodeForRender } = nodesForRender.find(({ check }) => check(node));
-      const { makeStringOfChanges } = stringPatterns.find(({ check }) => check(node.type));
+      const makeStringOfChanges = stringPatterns[node.type];
       const nodeForRender = makeNodeForRender(node, depth, iter);
       return makeStringOfChanges(nodeForRender, indent);
     });
